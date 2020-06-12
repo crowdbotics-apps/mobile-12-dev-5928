8ac88c20607a2257b92f52c3235eb2c0c653b761
Crowdbotics <team@crowdbotics.com>
2020-06-12 07:19:41 +0000

    remove package blueprint-react-native-message-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import CalendarView5468Navigator from '../features/CalendarView5468/navigator';
 import Messaging5467Navigator from '../features/Messaging5467/navigator';
 import EmailAuth5466Navigator from '../features/EmailAuth5466/navigator';
-import Messaging5461Navigator from '../features/Messaging5461/navigator';
 import CalendarView5459Navigator from '../features/CalendarView5459/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import CalendarNavigator from '../features/Calendar/navigator';
@@ -26,7 +25,6 @@ const AppNavigator = {
 CalendarView5468: { screen: CalendarView5468Navigator },
 Messaging5467: { screen: Messaging5467Navigator },
 EmailAuth5466: { screen: EmailAuth5466Navigator },
-Messaging5461: { screen: Messaging5461Navigator },
 CalendarView5459: { screen: CalendarView5459Navigator },
 Messenger: { screen: MessengerNavigator },
 Calendar: { screen: CalendarNavigator },
